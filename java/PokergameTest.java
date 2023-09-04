422bcfebc8358ea24862e8014c4a8af08f440d99
Ibrahim O. Ijaduola <ijaduola_io@cobra.cs.mercer.edu>
2023-09-03 21:49:51 -0400

    adding first piece of code

--- java/PokergameTest.java
+++ java/PokergameTest.java
@@ -2,9 +2,29 @@ import java.util.*;
 public class PokergameTest {
 
 	public static void main(String[] args) {
-		Cards card = new Cards('1', 'S');
+		Cards card = new Cards("1", 'S');
 		System.out.println(card);
 		
+   
+   Deck deck = new Deck();
+   deck.shuffle();
+   
+   
+   for (int i=0; i < 5; i++)
+   {
+     Cards card2 = deck.dealingCards();
+     if (card2 != null)
+     {
+       System.out.println("Dealing: " + card2);
+     }
+     else{
+       System.out.println("The deck is empty");
+       break;
+     
+     }
+   
+   
+   }
 
 	}
 
