98ce2830dcb8a1c1e5142fa8f5f640c47183ea04
Ibrahim O. Ijaduola <ijaduola_io@cobra.cs.mercer.edu>
2023-10-10 14:30:05 -0400

    Almost

--- python/Cards.py
+++ python/Cards.py
@@ -1,7 +1,213 @@
+from random import shuffle
+
+# Cards Class
 class Cards:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
 
+    def get_rank(self):
+        return self.rank
+
+    def get_suit(self):
+        return self.suit
+
+    def __lt__(self, other):
+        return self.rank < other.rank
+
     def __str__(self):
-        return f"{self.rank}, {self.suit}"
+        return str(self.rank) + str(self.suit)
+
+    @staticmethod
+    def get_rank_value(rank):
+        rank_values = {
+            "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
+            "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14
+        }
+        return rank_values.get(rank, -1)
+
+
+# Deck Class
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ['S', 'H', 'D', 'C']
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Cards(rank, suit))
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def deal_card(self):
+        if self.cards:
+            return self.cards.pop(0)
+        return None
+
+    def size(self):
+        return len(self.cards)
+
+    def print_deck(self):
+        count = 0
+        for card in self.cards:
+            print(card, "\t")
+            count += 1
+            if count % 13 == 0:
+                print()
+
+    def print_remaining_deck(self):
+        print("\nRemaining Cards in Deck:\t", "")
+        for card in self.cards:
+            print(card, "\t")
+        print()
+
+
+# Hand Class
+class Hand:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def get_cards(self):
+        return self.cards
+
+    def sort_hand(self):
+        self.cards.sort(key=lambda card: card.get_suit())
+
+    def __lt__(self, other):
+        return self.rank_hand() < other.rank_hand()
+
+    def print_hand(self):
+        count = 0
+        for card in self.cards:
+            print(card, "\t")
+            count += 1
+            if count % 5 == 0:
+                print()
+
+    def __str__(self):
+        hand_string = ""
+        count = 0
+        for card in self.cards:
+            hand_string += str(card) + "\t"
+            count += 1
+            if count % 5 == 0:
+                hand_string += "\n"
+        return hand_string
+
+    def rank_hand(self):
+        suit_counts = {}
+        rank_counts = {}
+        hand_rank = "Royal Straight Flush"
+
+        for card in self.cards:
+            suit = card.get_suit()
+            rank = card.get_rank()
+            suit_counts[suit] = suit_counts.get(suit, 0) + 1
+            rank_counts[rank] = rank_counts.get(rank, 0) + 1
+
+        if len(suit_counts) == 1 and len(rank_counts) == 5:
+            if self.is_royal_straight_flush(rank_counts.keys(), next(iter(suit_counts))):
+                return "Royal Straight Flush"
+            return "Straight Flush"
+
+        if len(suit_counts) == 1 and len(rank_counts) == 5:
+            if self.is_straight(rank_counts.keys()):
+                return "Straight Flush"
+            return "Flush"
+
+        if 4 in rank_counts.values():
+            return "Four of a Kind"
+
+        if 3 in rank_counts.values() and 2 in rank_counts.values():
+            return "Full House"
+
+        if len(rank_counts) == 5 and self.is_straight(rank_counts.keys()):
+            return "Straight"
+
+        if 3 in rank_counts.values():
+            return "Three of a Kind"
+
+        if 2 in rank_counts.values():
+            pair_count = sum(1 for count in rank_counts.values() if count == 2)
+            if pair_count == 2:
+                return "Two Pair"
+            else:
+                return "Pair"
+
+        return "High Card"
+
+    def is_royal_straight_flush(self, ranks, suit):
+        return all(rank in ranks for rank in ["10", "J", "Q", "K", "A"])
+
+    def is_straight(self, ranks):
+        rank_values = [Cards.get_rank_value(rank) for rank in ranks]
+        rank_values.sort()
+        min_rank = rank_values[0]
+        max_rank = rank_values[-1]
+
+        if max_rank - min_rank == 4 and len(ranks) == 5:
+            return True
+
+        if "A" in ranks and "2" in ranks and "3" in ranks and "4" in ranks and "5" in ranks:
+            return True
+
+        return False
+        
+        
+    def get_flush_suit(self):
+        suit_counts = {}
+        for card in self.cards:
+            suit = card.get_suit()
+            if suit in suit_counts:
+                suit_counts[suit] += 1
+            else:
+                suit_counts[suit] = 1
+
+        for suit, count in suit_counts.items():
+            if count >= 5:
+                return suit
+
+        return None  # No flush suit found
+
+# Player Class
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = Hand()
+
+    def add_to_hand(self, card):
+        self.hand.add_card(card)
+
+    def get_name(self):
+        return self.name
+
+    def get_hand(self):
+        return self.hand
+
+    def __str__(self):
+        return self.name
+
+
+# PlayerRank Class
+class PlayerRank:
+    def __init__(self, player, rank, suit):
+        self.player = player
+        self.rank = rank
+        self.suit = suit
+
+    def get_player(self):
+        return self.player
+
+    def get_rank(self):
+        return self.rank
+
+    def __lt__(self, other):
+        return self.rank < other.rank
+
+    def get_suit(self):
+        return self.suit
--- python/pokerTest.py
+++ python/pokerTest.py
@@ -1,6 +1,129 @@
+import sys
+import os
+from random import shuffle
 from Cards import Cards
+from Cards import Deck
+from Cards import Hand
+from Cards import Player
+from Cards import PlayerRank
 
-card1 = Cards("A", "S")
-print(card1)
+def compare_suits(suit1, suit2):
+    suit_order = "DCHS"  # The order is Diamonds, Clubs, Hearts, Spades
+    if suit1 is None:
+        return 1  # None is considered greater than any suit
+    if suit2 is None:
+        return -1  # None is considered greater than any suit
+    return suit_order.index(suit1) - suit_order.index(suit2)
+    
+# Function to read a deck from a file
+def read_deck_from_file(file_path):
+    all_hands = []
+    try:
+        with open(file_path, 'r') as reader:
+            lines = reader.readlines()
+            unique_cards = set()
+            print("File Name: " + file_path)
+            for line in lines:
+                print(line.strip())
+                card_records = line.strip().split(',')
+                if len(card_records) != 5:
+                    print("Invalid card record: " + str(card_records))
+                    print(len(card_records))
+                    continue
+                for card_record in card_records:
+                    if card_record in unique_cards:
+                        print(" *** ERROR - DUPLICATE CARD FOUND IN DECK *** ")
+                        print("DUPLICATE: " + card_record)
+                        sys.exit(1)
+                    unique_cards.add(card_record)
+                if len(card_records) == 5:
+                    new_hand = []
+                    for card_str in card_records:
+                        if len(card_str) == 2:
+                            rank = card_str[0]
+                            suit = card_str[1]
+                        elif len(card_str) == 3:
+                            rank = card_str[:2]
+                            suit = card_str[2]
+                        else:
+                            print("Invalid card format: " + card_str)
+                            continue
+                        card = Cards(rank, suit)
+                        new_hand.append(card)
+                    all_hands.append(new_hand)
+    except IOError as e:
+        print("Error reading the file: " + str(e))
+        sys.exit(1)
+
+    if len(all_hands) < 5:
+        print("Not enough cards to make a hand.")
+    else:
+        print("Here are the six hands...")
+        for i in range(6):
+            hand = all_hands[i]
+            print("Hand " + str(i + 1) + ":")
+            for card in hand:
+                print(str(card), "\t")
+            print()
+
+if __name__ == "__main__":
+    deck = Deck()
+    deck.shuffle()
+
+    if len(sys.argv) == 1:
+        print("*** USING RANDOMIZED DECK OF CARDS ***")
+        print()
+        print()
+        print("*** Shuffled 52 card deck: ")
+    else:
+        print("*** USING TEST DECK ***")  
+        file = sys.argv[1]
+        read_deck_from_file(file)
+        sys.exit(0)
+
+    num_players = 6
+    cards_per_hand = 5
+    new_hand = Hand()
+    deck.print_deck()
+
+    # Creating and dealing hands to players
+    players = [Player("Player " + str(i + 1)) for i in range(num_players)]
+
+    # Making a list to store objects of the class PlayerRank
+    player_ranks = []
+
+    for i in range(cards_per_hand):
+        for player in players:
+            new_card = deck.deal_card()
+            if new_card is not None:
+                player.add_to_hand(new_card)
+            else:
+                print("The deck is empty.")
+                break
+
+    player_ranks.sort()
+
+    # Calculating and storing ranks for each player's hand
+    print("Here are the six hands... ")
+    for new_player in players:
+        print(new_player.get_name() + " 's Hand:")
+        new_player.get_hand().print_hand()
+        hand_ranks = new_player.get_hand().rank_hand()
+        flush_suit = new_player.get_hand().get_flush_suit()
+        print()
+        player_ranks.append(PlayerRank(new_player, hand_ranks, flush_suit))
+
+    # Sorting players in order of first, then second, then third, etc...
+    player_ranks.sort(key=lambda pr: (-Cards.get_rank_value(pr.get_rank()), compare_suits(pr.get_suit(), pr.get_suit())))
+
+    # Printing out each hand and rank in order
+    print("//////////////////////////Winning Hand Order//////////////////////////////////////////////////////")
+    for new_player_rank in player_ranks:
+        print(new_player_rank.get_player().get_name() + " 's Hand:")
+        new_player_rank.get_player().get_hand().sort_hand()
+        print(" - " + new_player_rank.get_rank())
+        print()
+
+    deck.print_remaining_deck()
 
 
