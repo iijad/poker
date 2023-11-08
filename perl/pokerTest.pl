8aa31bb68d91f76b088819abb222e7aa8c7381d2
Ibrahim O. Ijaduola <ijaduola_io@cobra.cs.mercer.edu>
2023-11-07 22:30:48 -0500

    Need more

--- perl/pokerTest.pl
+++ perl/pokerTest.pl
@@ -6,7 +6,75 @@ use Deck;
 use Hand;
 use Player;
 
+my $file_path = $ARGV[0];
 
+# Check if a file path is provided as a command-line argument
+if (@ARGV == 1) {
+    # Check if the file exists
+    if (!-e $file_path) {
+        die "File not found: $file_path\n";
+    }
+
+    # Read the file and create cards
+    my %card_seen;
+    my @cards;
+    open my $file, '<', $file_path or die "Cannot open $file_path: $!\n";
+    while (my $line = <$file>) {
+        chomp $line;
+        next if $line eq '';  # Skip empty lines
+        my $card_str = $line;
+
+        if ($card_seen{$card_str}) {
+            die "Duplicate card found in the file: $card_str\n";
+        }
+
+        $card_seen{$card_str} = 1;
+        push @cards, $card_str;
+    }
+    close $file;
+
+    # Display the contents of the file
+    print "Contents of the file:\n";
+    foreach my $card_str (@cards) {
+        print "$card_str\n";
+    }
+    print "\n";
+
+    # Create 6 players with 5 cards each
+    my @players;
+    for my $i (1..6) {
+        my $player_name = "Player $i";
+        my $hand = Hand->new();
+        my @player_cards = splice(@cards, 0, 5);
+
+        if (scalar @player_cards != 5) {
+            die "Not enough cards for Player $i\n";
+        }
+
+        my $player = Player->new($player_name, Hand->new(@player_cards));
+        push @players, $player;
+    }
+
+    # Rank and print each player's hand
+    for my $player (@players) {
+        my $player_name = $player->getName();
+        my $player_hand = $player->getHand();
+
+        # Print the player's hand
+        print "$player_name's Hand:\n";
+        $player_hand->printHand();
+
+        # Rank and print the player's hand
+        my $hand_rank = $player_hand->rankHand();
+        print "$player_name's Hand Rank: $hand_rank\n";
+
+        print "\n";
+    }
+} elsif (@ARGV == 0) {
+    print "***USING RANDOM DECK***\n";
+} else {
+    die "Usage: perl program.pl [file_path]\n";
+}
 
 #testing the card deck
 my $deck = Deck->new();
